607fb1a1e518c8678eb699f2a8580009daa17d74
Andrey Pudov <mail@andreypudov.com>
2023-05-13 19:56:44 +0200

    Move comments to the beginning of the funciton definition

--- problems/problem-13.py
+++ problems/problem-13.py
@@ -1,4 +1,17 @@
 class Solution:
+    # M - 1000
+    # CM - 900
+    # D - 500
+    # CD - 400
+    # C - 100,
+    # XC - 90
+    # L - 50,
+    # XL - 40
+    # X - 10,
+    # IX - 9
+    # V - 5,
+    # IV - 4
+    # I - 1
     def romanToInt(self, s: str) -> int:
         number = 0
         idx = 0
@@ -44,19 +57,6 @@ class Solution:
                 number += 1
                 idx += 1
         return number
-            # M - 1000
-            # CM - 900
-            # D - 500
-            # CD - 400
-            # C - 100,
-            # XC - 90
-            # L - 50,
-            # XL - 40
-            # X - 10,
-            # IX - 9
-            # V - 5,
-            # IV - 4
-            # I - 1
 
     def getNextToken(self, s, pos):
         lenght = len(s)
