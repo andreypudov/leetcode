22b0b3325dcedccaaed07c87ff1d8647903e888c
Andrey Pudov <mail@andreypudov.com>
2023-09-30 13:09:55 +0200

    ~ updated readme

--- tests/test_problem_5.py
+++ tests/test_problem_5.py
@@ -9,8 +9,8 @@ class TestCase(unittest.TestCase):
         self.solution = Solution()
 
     def test_twoSum(self):
-        assert self.solution.longestPalindrome("babad") == "bab"
-        assert self.solution.longestPalindrome("cbbd") == "bb"
+        self.assertEqual(self.solution.longestPalindrome("babad"), "bab")
+        self.assertEqual(self.solution.longestPalindrome("cbbd"), "bb")
 
 
 if __name__ == "__main__":
