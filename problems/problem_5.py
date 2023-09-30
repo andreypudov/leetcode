9d69f13037c4852667565acdef3f6c1165cb24b4
Andrey Pudov <mail@andreypudov.com>
2023-09-30 10:46:43 +0200

    Problem 5

---
+++ problems/problem_5.py
@@ -0,0 +1,31 @@
+# Given a string s, return the longest palindromic substring in s.
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        palindrom = ""
+        length = 0
+
+        for index in range(len(s)):
+            cur_palindrom = self.getPalindrom(s[index:])
+            cur_length = len(cur_palindrom)
+
+            if cur_length > length:
+                palindrom = cur_palindrom
+                length = cur_length
+
+        return palindrom
+
+    def getPalindrom(self, s: str) -> str:
+        palindrom = ""
+
+        for index in range(len(s), 0, -1):
+            sub = s[0:index]
+            if self.isPalindrom(sub):
+                palindrom = sub
+                break
+
+        return palindrom
+
+    def isPalindrom(self, s: str) -> bool:
+        return s == s[::-1]
