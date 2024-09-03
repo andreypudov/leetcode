02c1d9e31bf89cd98e7fab979d6a65a383e716fe
Andrey Pudov <mail@andreypudov.com>
2024-09-03 20:40:37 +0200

    Placeholders for problem 16

---
+++ tests/test_problem_16.py
@@ -0,0 +1,13 @@
+import unittest
+
+from problems.problem_16 import Solution
+
+
+class TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestCase, self).__init__(*args, **kwargs)
+        self.solution = Solution()
+
+    def test_threeSumClosest(self):
+        self.assertEqual(self.solution.threeSumClosest([-1, 2, 1, -4], 1), 2)
+        self.assertEqual(self.solution.threeSumClosest([0, 0, 0], 1), 0)
