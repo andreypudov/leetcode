df187fbcd425f6ce4cec4e8b5834cc1af293b32e
Andrey Pudov <mail@andreypudov.com>
2024-09-03 21:15:26 +0200

    Problem 1757

---
+++ tests/test_problem_1757.py
@@ -0,0 +1,23 @@
+import unittest
+
+import pandas as pd
+
+from problems.problem_1757 import find_products
+
+
+class TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestCase, self).__init__(*args, **kwargs)
+        self.input = {
+            "product_id": [0, 1, 2, 3, 4],
+            "low_fats": ["Y", "Y", "N", "Y", "N"],
+            "recyclable": ["N", "Y", "Y", "Y", "N"],
+        }
+        self.expected = {
+            "product_id": [1, 3],
+        }
+
+    def test_find_products(self):
+        actual = find_products(pd.DataFrame(self.input))
+        expected = pd.DataFrame(self.expected)
+        self.assertEqual(actual.to_dict("records"), expected.to_dict("records"))
