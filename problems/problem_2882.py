24f1ef42794652f19434a68697f5c0974efe2059
Andrey Pudov <mail@andreypudov.com>
2024-09-14 22:33:54 +0200

    Problem 2882

---
+++ problems/problem_2882.py
@@ -0,0 +1,22 @@
+# 2882. Drop Duplicate Rows
+#
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | customer_id | int    |
+# | name        | object |
+# | email       | object |
+# +-------------+--------+
+#
+# There are some duplicate rows in the DataFrame based on the email column.
+#
+# Write a solution to remove these duplicate rows and keep only the first
+# occurrence.
+#
+# The result format is in the following example.
+
+import pandas as pd
+
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates("email")
