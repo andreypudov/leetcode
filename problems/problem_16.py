9276d491f696777335c87a028563a6cdde4e8565
Andrey Pudov <mail@andreypudov.com>
2024-09-28 22:04:04 +0200

    Problem 16

--- problems/problem_16.py
+++ problems/problem_16.py
@@ -12,4 +12,30 @@ from typing import List
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        pass
+        closest = float("inf")
+
+        nums.sort()
+
+        for index in range(len(nums)):
+            start = index + 1
+            end = len(nums) - 1
+            current = nums[index]
+
+            while start < end:
+                sum = current + nums[start] + nums[end]
+                closest = self.__update_closest(closest, sum, target)
+
+                if sum == target:
+                    return sum
+                elif sum < target:
+                    start += 1
+                else:
+                    end -= 1
+
+        return closest
+
+    def __update_closest(self, closest: int, sum: int, target: int) -> int:
+        if abs(target - sum) < abs(target - closest):
+            return sum
+
+        return closest
--- problems/tests/test_problem_16.py
+++ problems/tests/test_problem_16.py
@@ -11,3 +11,7 @@ class TestCase(unittest.TestCase):
     def test_threeSumClosest(self):
         self.assertEqual(self.solution.threeSumClosest([-1, 2, 1, -4], 1), 2)
         self.assertEqual(self.solution.threeSumClosest([0, 0, 0], 1), 0)
+        self.assertEqual(
+            self.solution.threeSumClosest([4, 0, 5, -5, 3, 3, 0, -4, -5], -2),
+            -2,
+        )
