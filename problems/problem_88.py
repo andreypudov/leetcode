035e772143b213c4636c998e66cfbcc44ae91be4
Andrey Pudov <mail@andreypudov.com>
2024-10-02 09:04:52 +0200

    Updated Problem 88

--- problems/problem_88.py
+++ problems/problem_88.py
@@ -21,12 +21,18 @@ class Solution:
         n -= 1
         w = m + n + 1
 
-        while n >= 0:
-            if m >= 0 and nums1[m] > nums2[n]:
-                nums1[w] = nums1[m]
-                m -= 1
-            else:
+        while m >= 0 and n >= 0:
+            if nums1[m] < nums2[n]:
                 nums1[w] = nums2[n]
                 n -= 1
+            else:
+                nums1[w] = nums1[m]
+                m -= 1
 
             w -= 1
+
+        if n >= 0:
+            nums1[0 : n + 1] = nums2[0 : n + 1]
+
+        if m >= 0:
+            nums2[0 : m + 1] = nums1[0 : m + 1]
