677b3714c3d334909122893bfba25a140208faff
Andrey Pudov <mail@andreypudov.com>
2024-10-20 14:04:14 +0200

    Problem 27

--- problems/tests/test_problem_27.py
+++ problems/tests/test_problem_27.py
@@ -10,12 +10,12 @@ class TestCase(unittest.TestCase):
         self.solution = Solution()
 
     def test_removeElement(self):
-        self.__removeElement([3, 2, 2, 3], 3, [2, 2, 3, 3], 2)
+        self.__removeElement([3, 2, 2, 3], 3, [2, 2, 2, 3], 2)
         self.__removeElement(
-            [0, 1, 2, 2, 3, 0, 4, 2], 2, [0, 1, 4, 0, 3, 2, 2, 2], 5
+            [0, 1, 2, 2, 3, 0, 4, 2], 2, [0, 1, 3, 0, 4, 0, 4, 2], 5
         )
         self.__removeElement([], 0, [], 0)
-        self.__removeElement([1], 1, [1], 1)
+        self.__removeElement([1], 1, [1], 0)
 
     def __removeElement(
         self,
