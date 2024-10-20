2a78bc9936acad995364f4572e11fce778b36e63
Andrey Pudov <mail@andreypudov.com>
2024-10-20 14:01:06 +0200

    Problem 27

---
+++ problems/tests/test_problem_27.py
@@ -0,0 +1,30 @@
+import unittest
+from typing import List
+
+from problems.problem_27 import Solution
+
+
+class TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestCase, self).__init__(*args, **kwargs)
+        self.solution = Solution()
+
+    def test_removeElement(self):
+        self.__removeElement([3, 2, 2, 3], 3, [2, 2, 3, 3], 2)
+        self.__removeElement(
+            [0, 1, 2, 2, 3, 0, 4, 2], 2, [0, 1, 4, 0, 3, 2, 2, 2], 5
+        )
+        self.__removeElement([], 0, [], 0)
+        self.__removeElement([1], 1, [1], 1)
+
+    def __removeElement(
+        self,
+        nums: List[int],
+        val: int,
+        expected_nums: List[int],
+        expected_k: int,
+    ) -> None:
+        k = self.solution.removeElement(nums, val)
+
+        self.assertEqual(k, expected_k)
+        self.assertEqual(nums, expected_nums)
