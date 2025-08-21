38f4b514809f5cf6c2194e2bf900c0c589177d93
Andrey Pudov <mail@andreypudov.com>
2025-08-21 21:13:34 +0200

    Co-authored-by: Pei Pei Chen <peipeichenml@users.noreply.github.com>

--- problems/problem_167.py
+++ problems/problem_167.py
@@ -18,4 +18,17 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        pass
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            sum = numbers[left] + numbers[right]
+
+            if sum < target:
+                left += 1
+            elif sum > target:
+                right -= 1
+            else:
+                return [left + 1, right + 1]
+
+        raise Exception("Invalid test case")
