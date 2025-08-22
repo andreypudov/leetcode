13ffa24a1e3e0b6771a3fe108cd0b202bab45530
Andrey Pudov <mail@andreypudov.com>
2025-08-22 16:17:01 +0200

    ~ added problem descriptions

---
+++ problems/problem_73.py
@@ -0,0 +1,17 @@
+# 73. Set Matrix Zeroes
+#
+# Given an m x n integer matrix matrix, if an element is 0, set its entire row
+# and column to 0's.
+#
+# You must do it in place.
+
+from typing import List
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        pass
