7f1cd0ff13e7f7c6a6ece9ae1cf1f69c48fd0c77
Andrey Pudov <mail@andreypudov.com>
2025-08-22 15:52:40 +0200

    ~ added problem descriptions

---
+++ problems/problem_42.py
@@ -0,0 +1,11 @@
+# 42. Trapping Rain Water
+#
+# Given n non-negative integers representing an elevation map where the width
+# of each bar is 1, compute how much water it can trap after raining.
+
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        pass
