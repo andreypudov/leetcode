aa234f2bff88c1f91ac1cf39c5c6a0eacd44f3dd
Andrey Pudov <mail@andreypudov.com>
2025-10-14 09:17:51 +0200

    Problem 380

--- problems/problem_380.py
+++ problems/problem_380.py
@@ -22,30 +22,33 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.current_list = []
-        self.index_memory = {}
+        self.items = []
+        self.positions = {}
         self.rng = random.Random(42)
 
     def insert(self, val: int) -> bool:
-        if val in self.index_memory:
+        if val in self.positions:
             return False
         else:
-            self.current_list.append(val)
-            self.index_memory[val] = len(self.current_list) - 1
+            self.items.append(val)
+            self.positions[val] = len(self.items) - 1
+
             return True
 
     def remove(self, val: int) -> bool:
-        if val in self.index_memory:
-            if self.index_memory[val] == len(self.current_list) - 1:
-                self.current_list.pop()
-            else:
-                last_value = self.current_list.pop()
-                self.current_list[self.index_memory[val]] = last_value
-                self.index_memory[last_value] = self.index_memory[val]
-            del self.index_memory[val]
+        if val in self.positions:
+            position = self.positions[val]
+            last_val = self.items[-1]
+
+            self.items[position] = last_val
+            self.positions[last_val] = position
+
+            self.items.pop()
+            del self.positions[val]
+
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return self.rng.choice(self.current_list)
+        return self.rng.choice(self.items)
